52e7702ce13e5d716d022f46b7181c1927c3ecf7
kaustubh739 <kaustubhwani95@gmail.com>
2025-05-10 23:28:12 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,7 @@
+# 1. Write a program which contains one function name as Fun(). that function should display "Hello from Fun" on console.
+
+def Fun():
+    print("Hello from Fun")
+
+if __name__ == "__main__":
+    Fun()
