4cc516ac3800c4b5ad1c359e9f5ee1091f00c2eb
kaustubh739 <kaustubhwani95@gmail.com>
2025-09-18 01:46:13 +0530

    Assignment2_10.py

--- Assignment2_10.py
+++ Assignment2_10.py
@@ -4,6 +4,8 @@ no = int(input("enter a number"))
 
 sum = 0 
 
-while no > 0:
-    sum = no % 10
-    print(sum)
+while (no > 0):
+    Digit = no % 10
+    Sum = Sum + Digit
+    no = int(no / 10)
+print(sum)
