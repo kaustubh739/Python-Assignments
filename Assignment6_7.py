db26fb747e5602a849287d547ade03c403d4b922
kaustubh739 <kaustubhwani95@gmail.com>
2025-09-23 03:48:26 +0530

    Update Assignment6_7.py

--- Assignment6_7.py
+++ Assignment6_7.py
@@ -1,1 +1,16 @@
 # Accept 5 numbers from the user. Find and print the largest number.
+
+
+No1 = int(input("Enter first number : "))
+
+No2 = int(input("Enter second number : "))
+
+No3 = int(input("Enter third number : "))
+
+No4 = int(input("Enter fourth number : "))
+
+No5 = int(input("Enter fifth number : "))
+
+maximum = max(No1,No2,No3,No4,No5)
+
+print("Maximum number is : ",maximum)
