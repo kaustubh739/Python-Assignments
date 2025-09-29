8f878a6080a8612fd485ad6849ceb715c0502816
kaustubh739 <kaustubhwani95@gmail.com>
2025-09-30 04:37:53 +0530

    Update Assignment14_3.py

--- Assignment14_3.py
+++ Assignment14_3.py
@@ -1,18 +1,27 @@
 # Create a class Book with private attribute __price. add method to get and set the price. show encapsulation.
 
 class Book:
+    def __init__(self,price):
+        self._price = price
 
-    def __init__(self,A):
-        self.__price = A
-
-    def Display(self):
-        print(self.__price)
-
+    def get_price(self):
+        return self._price
+    
+    def set_price(self,new_price):
+        if new_price >= 0:
+            self._price = new_price
+        else:
+            print("price cannot be negative")
 
 def main():
     obj = Book(1000)
 
-    obj.Display()
+    print("Initial price : ",obj.get_price())
 
+    obj.set_price(500)
+    print("Updated price : ",obj.get_price())
+
+    #obj.Details()
+    
 if __name__ == "__main__":
     main()
